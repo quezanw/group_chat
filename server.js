3b34cac6dea2089b8594eee382221b52788756c8
quezan <quezanw@gmail.com>
2018-11-01 10:28:45 -0700

    created all socket event handlers/listeners on front/back end

--- server.js
+++ server.js
@@ -5,13 +5,50 @@ var app = express();
 const server = app.listen(8000)
 const io = require('socket.io')(server);
 
+var users = {};
+io.on('connection', function (socket) {
+  let user_id = socket.id;
 
-io.on('connection', function(socket) {
-    socket.emit('connected', { msg: 'You have been connected to the server' })
+  socket.emit('connected', {
+    msg: 'You have been connected to the server'
+  })
 
-    socket.on("user_connected", function (data) {
-        console.log(data.msg);
+  socket.on("user_connected", function (data) {
+    console.log(data.msg);
+  })
+
+  socket.on("new_user_connected", function (data) {
+    // console.log("data: "+ data.new_user_name)
+    users[user_id] = data.new_user_name;
+
+    // broadcasts to everyone except the new user that someone has connected
+    socket.broadcast.emit("new_user", {
+      name: data.new_user_name
+    });
+
+    // emits data back to the new user who has connected
+    socket.emit("display_all", {
+      users: users
+    });
+
+  });
+
+  // emits an event when a user disconnects 
+  socket.on("disconnect", function () {
+    let data = {
+      name: users[user_id]
+    }
+    delete users[user_id];
+    data.users = users;
+    socket.broadcast.emit("user_disconnected", data)
+  });
+
+  socket.on("message_posted", function (data) {
+    io.emit("post_new_message", {
+      msg: data.msg,
+      user_name: users[user_id]
     })
+  });
 
 });
 
@@ -19,5 +56,5 @@ io.on('connection', function(socket) {
 app.use(express.static(path.join(__dirname, "./static")));
 
 app.get('/', function (request, response) {
-    response.render("index");
+  response.render("index");
 });
